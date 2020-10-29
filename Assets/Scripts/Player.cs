e80a726dfe7eedf52c4bed53741d7c80b61e7776
lingmengyanran <2251330050@qq.com>
2020-10-29 16:11:34 +0800

    提交信息

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("hujiahui");
+    }
+    // Start is called before the first frame update
+    Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        Debug.Log("liwei修改");
+        Debug.Log("liwei再次修改了");
+        Debug.Log("hujiahui又修改了");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
